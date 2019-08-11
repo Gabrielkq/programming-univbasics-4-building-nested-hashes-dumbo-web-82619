27181e9b58f44a826cd55cac436871ead7f43ee6
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-11 20:27:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,11 @@ def monopoly_with_third_tier
     :pieces=>4,
     :rent_in_dollars=>
     {
+<<<<<<< HEAD
       :one_piece_owned=>25,
+=======
+      :one_piece_ownd=>25,
+>>>>>>> 17dc1ad939990d06de3afe29692a4ab56c459d02
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
